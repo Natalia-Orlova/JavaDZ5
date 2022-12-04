44855b2e9b83e3fbab93c67f18c440aab79b11d6
NataliaOrlova <natakarelskaya@yandex.ru>
2022-12-04 23:50:05 +0400

    task2 нужно доработать

--- src/lesson5/TaskTwo.java
+++ src/lesson5/TaskTwo.java
@@ -1,4 +1,35 @@
 package lesson5;
 
+import java.util.*;
+
 public class TaskTwo {
-}
+//    public static void main(String[] args) {
+//        String[] words1 = new String[]{"leetcode", "is", "amazing", "as", "is"};
+//        String[] words2 = new String[]{"amazing", "leetcode", "is"};
+//
+//        System.out.println(countWords(words1, words2));
+//        System.out.println((count(words1)));
+//    }
+//
+//    public static int countWords(String[] words1, String[] words2) {
+//        if (words1.length > words2.length) {
+//            return countWords(words2, words1);
+//        }
+//        int answer = 0;
+//        for (String word : words1) {
+//            if (count(words1).getOrDefault(word, 0) == 1
+//                    && count(words2).getOrDefault(word, 0) == 1) {
+//                answer += 1;
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    private static Map<String, Integer> count(String[] words) {
+//        Map<String, Integer> count = new HashMap<>();
+//        for (String word : words) {
+//            count.put(word, count.getOrDefault(word, 0) + 1);
+//        }
+//        return count;
+//    }
+//}
